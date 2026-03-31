5821cfe5718614b161d3cfbf9686823ebe5dfdfc
whiezzer <endyturrin@gmail.com>
2026-03-31 16:51:08 +0200

    (Add) Project to create 3D house assets

--- script.js
+++ script.js
@@ -23,43 +23,47 @@ document.addEventListener("DOMContentLoaded", () => {
       "label-email": "Email",
       "label-message": "Message",
       "invaders-goal": "J'ai appris à utiliser PyGame et j'ai essayé de créer un premier jeu vidéo en Python afin de m'entraîner et de développer mes compétences en utilisant de nouveaux modules.",
-      "invaders-tools": "Outils : Thonny",
+      "invaders-tools": "Outil : Thonny",
       "invaders-lang": "Langage de programmation : Python",
-      "invaders-time": "Temps : 12h",
+      "invaders-time": "Temp : 12h",
       "platformer": "Platformer 2D",
       "platformer-goal": "J'ai créé mon premier platformer 2D à l’aide de tutos YouTube et j'ai apprie à maîtriser le C# ainsi que Unity.",
-      "platformer-tools": "Outils : Unity",
+      "platformer-tools": "Outil : Unity",
       "platformer-lang": "Langage de programmation : C#",
-      "platformer-status": "Temps : 7 jours",
+      "platformer-status": "Temp : 7 jours",
       "ShootEm_Up": "1er projet Unity",
       "shootemup-goal": "J'ai créé un premier projet Unity afin d'apprendre les bases du moteur de jeux à l'aide de tutos YouTube, et j'ai testé des idées qui me sont passées par la tête afin de m'entraîner.",
-      "shootemup-tools": "Outils : Unity",
+      "shootemup-tools": "Outil : Unity",
       "shootemup-lang": "Langage de programmation : C#",
-      "shootemup-status": "Temps : 4h",
+      "shootemup-status": "Temp : 4h",
       "groupe_NSI": "projet de groupe NSI",
       "nsi-goal": "J'ai fait un projet Python en groupe afin d'apprendre à m'organiser en codant à plusieurs et j'ai appris à utiliser le module Turtle.",
-      "nsi-tools": "Outils : Thonny",
+      "nsi-tools": "Outil : Thonny",
       "nsi-lang": "Langage de programmation : Python",
-      "nsi-time": "Temps : 3h",
+      "nsi-time": "Temp : 3h",
       "JAM-goal-1": "J'ai eu une première expérience de conception de jeu en groupe avec des masters dans le cadre de l'ENJAM, la GameJam annuelle de l'ENJMIN.",
       "JAM-goal-2": "J'ai appris les différentes étapes de la conception d'un jeu, les différents corps de métier et j'ai développé mes compétences en level design, en game design et en programmation sur Unity. Enfin j'ai appris les bases du métier de sound designer.",
-      "JAM-tools": "Outils : Unity",
+      "JAM-tools": "Outil : Unity",
       "JAM-lang": "Langage de programmation : C#",
-      "JAM-time": "Temps : 3 days",
+      "JAM-time": "Temp : 3 days",
       "JAM-link": "Page itch: ",
       "Mars2-goal": "Mon premier projet inter-Licence au sein du CNAM Enjmin à Angoulême. Un petit jeu de gestion dans lequel on gère une colonie alienne et où on doit faire augmenter la barre de prospérité de celle-ci. J'ai aidé principalement à la création de l'UI, du level design, du GameManager et à la gestion des personnages.",
-      "Mars2-tools": "Outils : Unity",
+      "Mars2-tools": "Outil : Unity",
       "Mars2-lang": "Langage de programmation : C#",
-      "Mars2-time": "Temps : 1 semaine",
+      "Mars2-time": "Temp : 1 semaine",
       "Editeur-perso-title" : "Editeur de perso 3D",
       "Editeur-perso-goal": "Un projet de Jam réalisé en deux semaines dans le cadre de mes cours au sein du CNAM Enjmin. J'ai réalisé la totalité de la partie programmation du projet et j'ai été aidé par deux artistes qui ont réalisé les assets visuels du personnage, du menu, de la skybox ainsi que du piédestal qui supporte le personnage.",
-      "Editeur-perso-tools": "Outils : Unreal",
+      "Editeur-perso-tools": "Outil : Unreal",
       "Editeur-perso-lang": "Système de scripting visuel : Blueprint",
-      "Editeur-perso-time": "Temps : 2 semaine",
+      "Editeur-perso-time": "Temp : 2 semaine",
       "groupe_village": "projet de groupe Village fortifié 3D",
       "village-goal": "J'ai fais un projet de groupe où on devait réaliser un village fortifié à l'aide d'assets qu'on devait réaliser nous même sur blender dans le cadre de mes cours au sein du CNAM-ENJMIN. Voici ce que j'ai pu réaliser.",
-      "village-tools": "Outils : Blender",
-      "village-time": "Temps : 3h",
+      "village-tools": "Outil : Blender",
+      "village-time": "Temp : 3h",
+      "Assets_House": "projet création d'assets d'une maison en 3D",
+      "House-goal": "J’ai commencé un projet personnel dans lequel je reproduis ma maison sur Blender, afin de mettre en pratique tout ce que j’ai appris récemment grâce à des tutoriels en ligne. Voici ce que j'ai pu réaliser pour l'instant.",
+      "House-tools": "Outil : Blender",
+      "House-time": "Temp : 4h",
       "solo": "(Seul)",
       "groupe": "(En groupe)",
       "code" : "Code du jeu :",
@@ -100,22 +104,22 @@ document.addEventListener("DOMContentLoaded", () => {
       "label-email": "Email",
       "label-message": "Message",
       "invaders-goal": "I learned how to use PyGame and tried to create my first video game in Python to practice and develop my skills using new modules.",
-      "invaders-tools": "Tools : Thonny",
+      "invaders-tools": "Tool : Thonny",
       "invaders-lang": "Programming language : Python",
       "invaders-time": "Time : 12h",
       "platformer": "2D Platformer",
       "platformer-goal": "I created my first 2D platformer using YouTube tutorials and I learned to master C# as well as Unity.",
-      "platformer-tools": "Tools : Unity",
+      "platformer-tools": "Tool : Unity",
       "platformer-lang": "Programming language : C#",
       "platformer-status": "Time : 7 days",
       "ShootEm_Up": "First Unity project",
       "shootemup-goal": "I created a first Unity project to learn the basics of the game engine using YouTube tutorials, and I tested ideas that came to mind to practice.",
-      "shootemup-tools": "Tools : Unity",
+      "shootemup-tools": "Tool : Unity",
       "shootemup-lang": "Programming language : C#",
       "shootemup-status": "Time : 4h",
       "groupe_NSI": "NSI group project",
       "nsi-goal": "I did a Python project in a group to learn how to organize myself when coding together and I learned how to use the Turtle module.",
-      "nsi-tools": "Tools : Thonny",
+      "nsi-tools": "Tool : Thonny",
       "nsi-lang": "Programming language : Python",
       "nsi-time": "Time : 3h",
       "JAM-goal-1": "I had my first experience designing a game in a group with master's students as part of the ENJAM, the annual Game Jam of the ENJMIN.",
@@ -124,18 +128,22 @@ document.addEventListener("DOMContentLoaded", () => {
       "JAM-time": "Time : 3 days",
       "JAM-link": "itch page : ",
       "Mars2-goal": "My first inter-license project at CNAM Enjmin in Angoulême. A small management game in which you manage an alien colony and have to increase its prosperity bar. I mainly helped with the creation of the UI, level design, GameManager, and character management.",
-      "Mars2-tools": "Tools : Unity",
+      "Mars2-tools": "Tool : Unity",
       "Mars2-lang": "Programming language : C#",
       "Mars2-time": "Time : 1 week",
       "Editeur-perso-title" : "3D character editor",
       "Editeur-perso-goal": "A Jam project completed in two weeks as part of my courses at CNAM Enjmin. I handled all the programming for the project and was assisted by two artists who created the visual assets for the character, the menu, the skybox, and the pedestal that supports the character.",
-      "Editeur-perso-tools": "Tools : Unreal",
+      "Editeur-perso-tools": "Tool : Unreal",
       "Editeur-perso-lang": "Visual scripting system : Blueprint",
       "Editeur-perso-time": "Time : 2 week",
       "groupe_village": "3D Fortified Village Group Project",
       "village-goal": "I worked on a group project where we had to create a fortified village using assets that we had to create ourselves in Blender as part of my coursework at CNAM-ENJMIN. Here's what I was able to create.",
-      "village-tools": "Tools : Blender",
+      "village-tools": "Tool : Blender",
       "village-time": "Time : 3h",
+      "Assets_House": "Project to create 3D house assets",
+      "House-goal": "I've started a personal project where I'm recreating my house in Blender, in order to put into practice everything I've recently learned from online tutorials. Here's what I've managed to achieve so far.",
+      "House-tools": "Tool : Blender",
+      "House-time": "Time : 4h",
       "solo": "(Alone)",
       "groupe": "(In group)",
       "code" : "Game code :",
@@ -245,23 +253,43 @@ window.onclick = function(event) {
   }
 }
 
-const images = [
+const imagesVillage = [
   "./assets/Images/RenduMurails.png",
   "./assets/Images/RenduTours.png",
 ];
 
-let index = 0;
+const imagesHouse = [
+  "./assets/Images/RenduAssetHouse-general.png",
+  "./assets/Images/RenduAssetHouse-meuble1.png",
+  "./assets/Images/RenduAssetHouse-meuble2.png",
+  "./assets/Images/RenduAssetHouse-lit.png",
+  "./assets/Images/RenduAssetHouse-porte.png",
+  "./assets/Images/RenduAssetHouse-bureau.png",
+];
 
-const img = document.getElementById("imageVillage");
-const left = document.getElementById("left");
-const right = document.getElementById("right");
+const leftVillage = document.getElementById("leftVillage");
+const rightVillage = document.getElementById("rightVillage");
+const imgVillage = document.getElementById("imageVillage");
 
-right.addEventListener("click", () => {
-  index = (index + 1) % images.length;
-  img.src = images[index];
-});
+const leftHouse = document.getElementById("leftHouse");
+const rightHouse = document.getElementById("rightHouse");
+const imgHouse = document.getElementById("imageHouse");
 
-left.addEventListener("click", () => {
-  index = (index - 1 + images.length) % images.length;
-  img.src = images[index];
-});
+function createCarousel(leftBtn, rightBtn, img, images) {
+  let index = 0; 
+
+  img.src = images[index]; 
+
+  rightBtn.addEventListener("click", () => {
+    index = (index + 1) % images.length;
+    img.src = images[index];
+  });
+
+  leftBtn.addEventListener("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    img.src = images[index];
+  });
+}
+
+createCarousel(leftVillage, rightVillage, imgVillage, imagesVillage);
+createCarousel(leftHouse, rightHouse, imgHouse, imagesHouse);
